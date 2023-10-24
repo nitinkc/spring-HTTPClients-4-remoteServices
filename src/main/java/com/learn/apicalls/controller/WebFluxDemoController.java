bf60123d2dd4ec3d17de0e3c2dd2a1cb8e024aa5
Nitin Chaurasia <gs.nitin@gmail.com>
2023-10-23 23:47:44 -0600

    updates

--- src/main/java/com/learn/apicalls/controller/WebFluxDemoController.java
+++ src/main/java/com/learn/apicalls/controller/WebFluxDemoController.java
@@ -3,11 +3,12 @@ package com.learn.apicalls.controller;
 import com.learn.apicalls.model.Beers;
 import com.learn.apicalls.service.webFluxService.WebFluxService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class WebFluxDemoController {
@@ -22,6 +23,15 @@ public class WebFluxDemoController {
     public void getBeersWebFlux(@RequestParam(value = "size",required = true, defaultValue = "10") Integer size){
         webFluxService.getResponseUsingWebFlux();
         return ;
+    }
+
+    @PostMapping("/post/test")
+    public void getData(@RequestBody Map<String, Object> requestBody){
+        try {
+            webFluxService.getPostResponseWebFlux(requestBody);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- src/main/java/com/learn/apicalls/service/webFluxService/WebFluxService.java
+++ src/main/java/com/learn/apicalls/service/webFluxService/WebFluxService.java
@@ -1,7 +1,15 @@
 package com.learn.apicalls.service.webFluxService;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
 
 @Service
 public class WebFluxService {
@@ -15,4 +23,51 @@ public class WebFluxService {
 
     }
 
+    public void getPostResponseWebFlux(Map<String, Object> requestBodyFromClient) throws URISyntaxException {
+
+        MultiValueMap<String, String> requestBodyCustom = new LinkedMultiValueMap<>();
+
+        requestBodyCustom.add("name", "John Dow");
+        requestBodyCustom.add("occupation", "timepass");
+
+        String response = getFromReqBodyJSON(requestBodyFromClient);
+
+        System.out.printf(response);
+    }
+
+    private String getFromReqBodyJSON(Map<String, Object> requestBodyFromClient) throws URISyntaxException {
+        return client.post()
+                .uri(new URI("https://httpbin.org/post"))
+                .header("Authorization", "Bearer MY_SECRET_TOKEN")
+                .contentType(MediaType.APPLICATION_JSON)//CHECK THIS
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(requestBodyFromClient)//CHECK THIS
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    private String getFromReqBodyFORM(Map<String, Object> requestBodyFromClient) throws URISyntaxException {
+        return client.post()
+                .uri(new URI("https://httpbin.org/post"))
+                .header("Authorization", "Bearer MY_SECRET_TOKEN")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(requestBodyFromClient.toString())//ACCEPTS STRING
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    private String getFromReqBodyLocasl(MultiValueMap<String, String> requestBodyFromJava) throws URISyntaxException {
+        return client.post()
+                .uri(new URI("https://httpbin.org/post"))
+                .header("Authorization", "Bearer MY_SECRET_TOKEN")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromFormData(requestBodyFromJava))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
 }
